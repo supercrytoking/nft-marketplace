713a7f6acaf65c20f73a9b5657d09fd3a8747fe4
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2022-01-06 18:50:21 -0500

    Added Notice

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,26 @@ import Link from 'next/link'
 
 export default function Header() {
     return (
-        <div className="px-6 pt-12 max-w-7xl mx-auto flex items-center space-x-4">
-            <Link href="/" passHref>
-                <a>Home</a>
-            </Link>
-            <Link href="/explore" passHref>
-                <a>Explore</a>
-            </Link>
-            <Link href="/create" passHref>
-                <a>Create</a>
-            </Link>
-            <Link href="/wallet" passHref>
-                <a>Wallet</a>
-            </Link>
-        </div>
+        <>
+            <div className="bg-blue-500">
+                <div className="px-6 py-2 max-w-7xl mx-auto flex items-center space-x-4">
+                    <p className="text-xs">Notice: This software is in beta and may be buggy. Use at your own risk.</p>
+                </div>
+            </div>
+            <div className="px-6 pt-12 max-w-7xl mx-auto flex items-center space-x-4">
+                <Link href="/" passHref>
+                    <a>Home</a>
+                </Link>
+                <Link href="/explore" passHref>
+                    <a>Explore</a>
+                </Link>
+                <Link href="/create" passHref>
+                    <a>Create</a>
+                </Link>
+                <Link href="/wallet" passHref>
+                    <a>Wallet</a>
+                </Link>
+            </div>
+        </>
     )
 }
