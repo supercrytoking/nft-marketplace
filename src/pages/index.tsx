de13b9d2bd86688c103a1e4b66af2bc60608c1c8
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2022-01-06 13:04:10 -0500

    Added Bnaneer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,10 @@ import Link from 'next/link'
 import { formatPricing } from '../utils/utils'
 
 export default function IndexPage() {
-    const { data: listings } = useSWR('/listings')
-
     return (
         <div className="p-6 max-w-7xl mx-auto">
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {listings?.map((listing) => (
-                    <Link href={`/${listing.contractAddress}/${listing.tokenId}`}>
-                        <a className="block">
-                            <img src={listing.metadata} alt="" />
-                            <div className="bg-white text-black p-2">
-                                <p>{listing.metadata.name}</p>
-                                <p className="text-xs opacity-50">{formatPricing(listing.listing.price)}</p>
-                            </div>
-                        </a>
-                    </Link>
-                ))}
+            <div className="bg-white text-black h-96 flex items-center justify-center">
+                <p className="text-2xl">Welcome to a new kind of marketplace.</p>
             </div>
         </div>
     )
