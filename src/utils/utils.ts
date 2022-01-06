9d4d158415b3e4f08a547e9dbfdfb5c4101f012b
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2022-01-06 06:33:48 -0500

    Fixed API URL

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,5 +5,4 @@ import axios from 'axios'
 
 export const formatPricing = (price) => `${Number(price) > 10000 ? shortNumber(Number(Web3.utils.fromWei(price))) : commaNumber(Number(Web3.utils.fromWei(price)))} FTM`
 
-// export const api = axios.create({ baseURL: 'http://api.fantom.digital/' })
-export const api = axios.create({ baseURL: 'http://localhost:8888/' })
+export const api = axios.create({ baseURL: process.env.NODE_ENV === 'production' ? 'https://api.fantom.digital/' : 'http://localhost:8888/' })
