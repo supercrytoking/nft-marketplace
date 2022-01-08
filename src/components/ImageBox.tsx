b3eace0b65190d174fd300fac3bd74ca44ad1e45
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2022-01-07 23:17:19 -0500

    Added reloading only on scrolled

--- src/components/ImageBox.tsx
+++ src/components/ImageBox.tsx
@@ -1,24 +1,30 @@
 import Link from 'next/link'
 import LazyLoad from 'react-lazyload'
+import TrackVisibility from 'react-on-screen'
 import { cacheImage } from '../utils/utils'
 
 export default function ImageBox({ nft }) {
     return (
-        <Link key={`${nft.contractAddress}-${nft.tokenId}`} href={`/${nft.contractAddress}/${nft.tokenId}`}>
-            <a className="rounded bg-zinc-900 border-zinc-800 border overflow-hidden flex items-center">
-                <LazyLoad once>
-                    <img
-                        className="w-full"
-                        src={cacheImage(nft.metadata.image.replace('ipfs://', 'https://ipfs.io/ipfs/'))}
-                        onError={(e) => {
-                            setTimeout(() => {
-                                e.target.src += `?${+new Date()}`
-                            }, 1000)
-                        }}
-                        alt=""
-                    />
-                </LazyLoad>
-            </a>
-        </Link>
+        <LazyLoad once>
+            <TrackVisibility>
+                {({ isVisible }) => (
+                    <Link key={`${nft.contractAddress}-${nft.tokenId}`} href={`/${nft.contractAddress}/${nft.tokenId}`}>
+                        <a className="rounded bg-zinc-900 border-zinc-800 border overflow-hidden flex items-center h-full" style={{ minHeight: '24rem' }}>
+                            {isVisible ? 'y' : 'n'}
+                            <img
+                                className=""
+                                src={cacheImage(nft.metadata.image.replace('ipfs://', 'https://ipfs.io/ipfs/'))}
+                                onError={(e) => {
+                                    setTimeout(() => {
+                                        if (isVisible) e.target.src += `?${+new Date()}`
+                                    }, 1000)
+                                }}
+                                alt=""
+                            />
+                        </a>
+                    </Link>
+                )}
+            </TrackVisibility>
+        </LazyLoad>
     )
 }
