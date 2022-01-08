6be572e24b05e183bf4359c7236fb55bfbfae0f1
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2022-01-08 15:19:04 -0500

    Fixed Mobile Sizing

--- src/components/ImageBox.tsx
+++ src/components/ImageBox.tsx
@@ -5,6 +5,7 @@ import useOnScreen from '../hooks/useOnScreen'
 import hash from 'hash-string'
 import { api, cacheImage } from '../utils/utils'
 import axios from 'axios'
+import classNames from 'classnames'
 
 export default function ImageBox({ nft }) {
     const imageId = hash(nft.metadata.image.replace('ipfs://', 'https://ipfs.io/ipfs/'))
@@ -17,18 +18,6 @@ export default function ImageBox({ nft }) {
     const boxRef = useRef()
     const ref = useRef()
     const isVisible = useOnScreen(boxRef)
-
-    const queueImage = async () => {
-        setRefreshing(true)
-        if (!refreshing) {
-            console.log('Queueing...')
-            try {
-                await api.get(cacheImage(nft.metadata.image.replace('ipfs://', 'https://ipfs.io/ipfs/')))
-            } catch (error) {
-                queueImage()
-            }
-        }
-    }
 
     useEffect(() => {
         if (!isVisible) return
@@ -69,9 +58,13 @@ export default function ImageBox({ nft }) {
     return (
         <>
             <Link key={`${nft.contractAddress}-${nft.tokenId}`} href={`/${nft.contractAddress}/${nft.tokenId}`}>
-                <a ref={boxRef} className="relative rounded bg-zinc-900 border-zinc-800 border overflow-hidden flex items-center justify-center h-full" style={{ minHeight: `24em` }}>
-                    {!isLoaded && <img className="absolute" src={'/img/loading.gif'} alt="" />}
-                    <img ref={ref} className="h-full relative" src={`data:image/jpeg;charset=utf-8;base64,${imageBinary}`} onLoad={() => setIsLoaded(true)} onError={(e) => setIsLoaded(false)} alt="" />
+                <a ref={boxRef} className={classNames('relative rounded bg-zinc-900 border-zinc-800 border overflow-hidden flex items-center justify-center h-full', !isLoaded && 'square')}>
+                    {!isLoaded && (
+                        <div className="content">
+                            <img className="" src={'/img/loading.gif'} alt="" />
+                        </div>
+                    )}
+                    <img ref={ref} className="" src={`data:image/jpeg;charset=utf-8;base64,${imageBinary}`} onLoad={() => setIsLoaded(true)} onError={(e) => setIsLoaded(false)} alt="" />
                 </a>
             </Link>
         </>
--- src/hooks/useOnScreen.tsx
+++ src/hooks/useOnScreen.tsx
@@ -4,6 +4,8 @@ export default function useOnScreen(ref, rootMargin = '0px') {
     // State and setter for storing whether element is visible
     const [isIntersecting, setIntersecting] = useState(false)
     useEffect(() => {
+        if (!ref.current) return
+
         const observer = new IntersectionObserver(
             ([entry]) => {
                 // Update our state when observer callback fires
@@ -13,12 +15,11 @@ export default function useOnScreen(ref, rootMargin = '0px') {
                 rootMargin
             }
         )
-        if (ref.current) {
-            observer.observe(ref.current)
-        }
+        observer.observe(ref.current)
         return () => {
+            if (!ref.current) return
             observer.unobserve(ref.current)
         }
-    }, []) // Empty array ensures that effect is only run on mount and unmount
+    }, [])
     return isIntersecting
 }
