d42cdb3b10fa367fad7e83527bb6f79ef7c7e721
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2022-06-09 22:19:57 -0400

    PRog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,9 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <Head>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
             </Head>
-            <NextNProgress color="white"
-            />
-
+            <NextNProgress color="white" />
             <UseWalletProvider>
                 <SWRConfig value={{
                     refreshInterval: 3000, fetcher: (url) => api.get(url).then((res) => res.data)
