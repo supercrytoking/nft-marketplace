c37f45307abd11b393004a846dd4e1e199c24ef0
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2022-06-09 20:00:41 -0400

    Added Loade

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,12 +8,16 @@ import '../styles/global.css'
 import { api } from '../utils/utils'
 import dayjs from 'dayjs'
 import Footer from '../components/Footer'
+import 'nprogress/nprogress.css'; //styles of nprogress
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
 
 const relativeTime = require('dayjs/plugin/relativeTime')
-
 dayjs.extend(relativeTime)
 
-const fetcher = (url) => api.get(url).then((res) => res.data)
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
@@ -23,7 +27,10 @@ export default function App({ Component, pageProps }: AppProps) {
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
             </Head>
             <UseWalletProvider>
-                <SWRConfig value={{ refreshInterval: 3000, fetcher }}>
+                <SWRConfig value={{
+                    refreshInterval: 3000, fetcher: (url) => api.get(url).then((res) => res.data)
+
+                }}>
                     <Header />
                     <Component {...pageProps} />
                     <Footer />
