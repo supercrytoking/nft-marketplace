e93a1bda26489896dc77881ffc903e8f08c15794
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2022-06-17 14:57:10 -0400

    Added Sentry!

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,20 @@
 import dayjs from 'dayjs'
 import type { AppProps } from 'next/app'
-import dynamic from 'next/dynamic'
 import Head from 'next/head'
+import NextNProgress from "nextjs-progressbar"
 import 'nprogress/nprogress.css'
 import { SWRConfig } from 'swr'
 import { UseWalletProvider } from 'use-wallet'
+import { AppWrapper } from '../AppWrapper'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Meta from '../components/Meta'
 import '../styles/global.css'
 import { api } from '../utils/utils'
-import NextNProgress from "nextjs-progressbar";
-import { AppWrapper } from '../AppWrapper'
 
 
 const relativeTime = require('dayjs/plugin/relativeTime')
 dayjs.extend(relativeTime)
-
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
